d6c7e0b0d6ae149d9ba08c597ac0cf3686eb8ce7
Vitaly-V <gtopbox@gmail.com>
2015-06-19 10:12:00 +0300

    Finish test app

--- _/js/myscript.js
+++ _/js/myscript.js
@@ -17,10 +17,62 @@
             //Resize and position overlay
             myOverlay.style.width = window.innerWidth + 'px';
             myOverlay.style.height = window.innerHeight + 'px';
+            myOverlay.style.top = window.pageYOffset + 'px';
+            myOverlay.style.left = window.pageXOffset + 'px';
+
+            //Create image element
+            var imageSrc = e.target.src;
+            var largeImage = document.createElement('img');
+            largeImage.src = imageSrc.substr(0, imageSrc.length -7) + '.jpg';
+            largeImage.style.display = 'block';
+            largeImage.style.position = 'absolute';
 
 
+            //Wait until the image has loaded
+            largeImage.addEventListener('load', function() {
+
+                //Resize if taller
+                if (this.height > window.innerHeight) {
+                    this.ratio = window.innerHeight / this.height;
+                    this.height = this.height * this.ratio;
+                    this.width = this.width * this.ratio;
+                }
+
+                //Resize if wider
+                if (this.width > window.innerWidth) {
+                    this.ratio = window.innerWidth / this.width;
+                    this.height = this.width * this.ratio;
+                    this.width = this.width * this.ratio;
+                }
+
+                cenerImage(this);
+                myOverlay.appendChild(largeImage);
+
+            }); //Image has loaded
+
+            largeImage.addEventListener('click', function(){
+                if (myOverlay) {
+                    myOverlay.parentNode.removeChild(myOverlay);
+                }
+            }, false);
+
+            window.addEventListener('scroll', function() {
+                if (myOverlay) {
+                    myOverlay.style.top = window.pageYOffset + 'px';
+                    myOverlay.style.left = window.pageXOffset + 'px';
+                }
+            }, false)
         } //Target is an image
 
     }, false); // Image clicking
 
+    function cenerImage(theImage) {
+        var myDifX = (window.innerWidth - theImage.width)/2;
+        var myDifY = (window.innerHeight - theImage.height)/2;
+
+        theImage.style.top = myDifY + 'px';
+        theImage.style.left = myDifX + 'px';
+
+        return theImage;
+    }
 })(); //Self executing function
